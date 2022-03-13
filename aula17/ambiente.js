1d87b166ada6203688168ab21bc5be3627ca1b34
marcelocassar <marcelocassar@gmail.com>
2022-03-13 15:41:23 -0300

    das

---
+++ aula17/ambiente.js
@@ -0,0 +1,38 @@
+var res = "	25/08/2015	"
+
+var x = 2
+
+switch (res) {
+    
+case "	25/08/2015	":	res = 	42240   
+break
+case 	"	24/08/2015	":	res =   42239   
+break
+case 	"	23/08/2015	":	res =   42238   
+break	
+case 	"	22/08/2015	":	res =   42237   
+break	
+case 	"	21/08/2015	":	res =   42236   
+break	
+case 	"	20/08/2015	":	res =   42235   
+break	
+case 	"	19/08/2015	":	res =   42234   
+break	
+case 	"	18/08/2015	":	res =   42233   
+break	
+case 	"	17/08/2015	":	res =   42232   
+break	
+case 	"	16/08/2015	":	res =   42231   
+break	
+case 	"	15/08/2015	":	res =   42230   
+break	
+case 	"	14/08/2015	":	res =   42229   
+break	
+default
+document.write("Não achou nada")
+
+}
+
+document.write (res)
+document.write (x)
+
