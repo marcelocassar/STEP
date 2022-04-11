27eb143f1855a513e6d9bbf603b9aeda45f54d4b
marcelocassar <marcelocassar@gmail.com>
2022-04-11 10:33:07 -0300

    das

---
+++ aula24/java.js
@@ -0,0 +1,43 @@
+function num1(){
+    
+}
+
+function num2(){
+    
+}
+
+function num3(){
+    
+}
+
+function num4(){
+    
+}
+
+function num5(){
+    
+}
+
+function num6(){
+    
+}
+
+function num7(){
+    
+}
+
+function num8(){
+    
+}
+
+function num9(){
+    
+}
+
+function num0(){
+    
+}
+
+function vigula(){
+    
+}
